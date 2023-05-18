7d511b1e17cdce8db31295046743a14bc461fe90
Rameik <116977233+Rameik@users.noreply.github.com>
2023-05-18 19:25:20 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_04_Bis.Models;
+
+namespace TP_04_Bis.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.ListaPaises = Info.listarPaises();
+        return View();
+    }
+
+    public IActionResult DetallePais(int idPais)
+    {
+        ViewBag.Pais = Info.detallePais(idPais);
+        return View();
+    }
+}
